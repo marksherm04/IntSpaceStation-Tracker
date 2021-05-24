469ffeec08962a80bcd54a346f36c574142c2f48
Mark S <marksherm04@gmail.com>
2021-05-24 17:14:10 -0400

    added fucntion into js for the location/speed data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
-var todayDate = moment();
-var displayTodayDate = document.getElementById("today-date");
+// moment.js date displaying in header
+const todayDate = moment();
+const displayTodayDate = document.getElementById("today-date");
+// Satellite API
+const apiSatelliteURL = "https://api.wheretheiss.at/v1/satellites/25544"
+const apiGoogleMaps = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAh3MAhVK8vAiE25mVZrwuB9XnqlS_rkoI&callback=initMap"
 
+
+
+function locationSpeedData(latitude, longitude, altitude, velocity) {
+document.getElementById("longitude").textContent = longitude;
+document.getElementById("latitude").textContent = latitude;
+document.getElementById("altitude").textContent = altitude;
+document.getElementById("velocity").textContent = velocity;
+};
+
+
+// today's date in header
 displayTodayDate.innerHTML = todayDate.format("LL");
-
-// Satellite API "where is ISS at?" 
-// https://api.wheretheiss.at/v1/coordinates/37.795517,-122.393693
