38c4d4e52483b46c09ef60ad0a04d3623395f4d8
Mark S <marksherm04@gmail.com>
2021-05-24 15:54:47 -0400

    added API key for google and added card space for satellite stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,6 @@ var todayDate = moment();
 var displayTodayDate = document.getElementById("today-date");
 
 displayTodayDate.innerHTML = todayDate.format("LL");
+
+// Satellite API "where is ISS at?" 
+// https://api.wheretheiss.at/v1/coordinates/37.795517,-122.393693
